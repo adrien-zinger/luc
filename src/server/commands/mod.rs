a51607cc8493e6cf0b2a194b1f0e50cb39518922
Adrien Zinger <zinger.ad@gmail.com>
2021-09-11 16:29:27 +0200

    Use match for the commands handling

--- src/server/commands/mod.rs
+++ src/server/commands/mod.rs
@@ -20,51 +20,39 @@ pub async fn handle(content: &str, stream: &mut TcpStream, globals: &Globals) ->
     }
     let action = act.unwrap();
     let current_ip = &globals.addr.lock().await.to_owned();
-    if action.name == "index" {
-        println!(
-            "index: {}",
-            format!("{:?}", globals.index.lock().await.to_vec())
-        );
-    } else if action.name == "luc" {
-        insert::insert(content, &globals.index, &globals.history).await;
-    } else if action.name == "p" {
-        print::print(content, &globals.index, &globals.history).await;
-    } else if action.name == "luc?" {
-        command_inter::commands_luc_inter(
-            action.option,
-            content,
-            &globals.index,
-            &globals.history,
-            &globals.addr.lock().await.to_owned(),
-            stream,
-        ) // todo send less objects
-        .await;
-        addr::addr(&globals.index).await;
-    } else if action.name == "connection" {
-        command_inter::command_connection(content, &globals.index).await;
-        addr::addr(&globals.index).await;
-    } else if action.name == "history" {
-        println!("{}", format!("{:?}", globals.history.lock().await.to_vec()))
-    } else if action.name == "group" {
-        group::create(action.option, current_ip).await;
-    } else if action.name == "invite" {
-        group::invite(action.option).await;
-    } else if action.name == "updategroup" {
-        group::update(action.option).await;
-    } else if action.name == "have" { // response to check or force update
-        group::have(action.option).await;
-    } else if action.name == "fetch" { // want or check
-        group::fetch(action.option).await;
-    } else if action.name == "receive" { // receive file from someone
-        group::receive(action.option).await;
-    } else if action.name == "findprop" {
-        find::propagate(action.option, &globals.index, &globals.history).await;
-    } else if action.name == "addr" {
-        if let Some(ip) = addr::addr(&globals.index).await {
+    match action.name {
+        "index" => println!("index: {}", format!("{:?}", globals.index.lock().await.to_vec())),
+        "luc" => insert::insert(content, &globals.index, &globals.history).await,
+        "p" => print::print(content, &globals.index, &globals.history).await,
+        "luc?" => {
+            command_inter::commands_luc_inter(
+                action.option,
+                content,
+                &globals.index,
+                &globals.history,
+                &globals.addr.lock().await.to_owned(),
+                stream,
+            ) // todo send less objects
+            .await;
+            addr::addr(&globals.index).await;
+        }
+        "connection" => {
+            command_inter::command_connection(content, &globals.index).await;
+            addr::addr(&globals.index).await;
+        }
+        "history" => println!("{}", format!("{:?}", globals.history.lock().await.to_vec())),
+        "group" => group::create(action.option, current_ip).await,
+        "invite" => group::invite(action.option).await,
+        "updategroup" => group::update(action.option).await,
+        "have" => group::have(action.option).await, // response to check or force update
+        "fetch" => group::fetch(action.option).await, // want or check
+        "receive" => group::receive(action.option).await, // receive file from someone
+        "findprop" => find::propagate(action.option, &globals.index, &globals.history).await,
+        "addr" => if let Some(ip) = addr::addr(&globals.index).await { // require my ip address
             *globals.addr.lock().await = ip;
         }
-    } else if action.name == "raddr" {
-        addr::raddr(stream).await;
-    }
+        "raddr" => addr::raddr(stream).await, // return the ip of the client
+        _ => {}
+    };
     action.name == "q"
 }
